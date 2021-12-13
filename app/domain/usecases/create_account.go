dcc9a80522333225e66a0d47570d0afe549ca01a
jpgsaraceni <jpgomes@id.uff.br>
2021-12-13 20:20:07 -0300

    refactor: Add validations and tests to usecase

--- app/domain/usecases/create_account.go
+++ app/domain/usecases/create_account.go
@@ -1,15 +1,53 @@
 package usecase
 
 import (
+	"errors"
+
 	"github.com/jpgsaraceni/suricate-bank/app/domain/entities"
 )
 
-func Create(name string, cpf entities.Cpf, secret string) (entities.Account, error) {
-	a, err := entities.NewAccount(name, cpf, secret)
+var (
+	errCreate = errors.New("failed to create account")
+	errName   = errors.New("invalid name length")
+	errCpf    = errors.New("invalid cpf length")
+	errSecret = errors.New("invalid password length")
+)
 
-	if err != nil {
-		return a, err
+const (
+	minNameLength = 3
+	maxNameLength = 100
+
+	rawCpfLength    = 11
+	maskedCpfLength = 14
+
+	minPasswordLength = 6
+)
+
+// CreateAccount checks if lengths of arguments are ok, then calls entities.NewAccount,
+// and returns the created Account struct.
+func CreateAccount(name, cpf, secret string) (entities.Account, error) {
+	var newAccount entities.Account
+
+	if len(name) < minNameLength || len(name) > maxNameLength {
+
+		return newAccount, errName
 	}
 
-	return a, nil
+	if len(cpf) != rawCpfLength || len(cpf) != maskedCpfLength {
+
+		return newAccount, errCpf
+	}
+
+	if len(secret) < minPasswordLength {
+
+		return newAccount, errSecret
+	}
+
+	newAccount, err := entities.NewAccount(name, cpf, secret)
+
+	if err != nil {
+		return newAccount, errCreate
+	}
+
+	return newAccount, nil
 }
---
+++ app/domain/usecases/create_account_test.go
@@ -0,0 +1,53 @@
+package usecase
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/jpgsaraceni/suricate-bank/app/domain/entities"
+)
+
+func TestCreateAccount(t *testing.T) {
+	t.Parallel()
+
+	type arg struct {
+		name   string
+		cpf    string
+		secret string
+	}
+
+	type testCase struct {
+		name string
+		args arg
+		want entities.Account
+		err  error
+	}
+
+	testCases := []testCase{
+		{
+			name: "name is too short",
+			args: arg{
+				name:   "me",
+				cpf:    "123.456.789-01",
+				secret: "123456",
+			},
+			err: errName,
+		},
+	}
+
+	for _, tt := range testCases {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got, err := CreateAccount(tt.args.name, tt.args.cpf, tt.args.secret)
+
+			if err != tt.err {
+				t.Errorf("got error %s expected error %s", err, tt.err)
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("got %v expected %v", got, tt.want)
+			}
+		})
+	}
+}
