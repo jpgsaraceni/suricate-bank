b93f5681899afb86c980484a5b811eea31ca3969
jpgsaraceni <jpgomes@id.uff.br>
2021-12-20 15:30:37 -0300

    feat: Create create_transfer usecase

--- app/domain/usecases/transfer/create_transfer.go
+++ app/domain/usecases/transfer/create_transfer.go
@@ -1,1 +1,25 @@
 package transferuc
+
+import (
+	"github.com/jpgsaraceni/suricate-bank/app/domain/entities/account"
+	"github.com/jpgsaraceni/suricate-bank/app/domain/entities/transfer"
+	"github.com/jpgsaraceni/suricate-bank/app/vos/money"
+)
+
+func (uc Usecase) Create(originId, destinationId account.AccountId, amount money.Money) (transfer.Transfer, error) {
+
+	newTransfer, err := transfer.NewTransfer(amount, originId, destinationId)
+
+	if err != nil {
+		return transfer.Transfer{}, ErrCreateTransfer
+	}
+
+	err = uc.Repository.Create(&newTransfer)
+
+	if err != nil {
+
+		return transfer.Transfer{}, ErrCreateTransferRepository
+	}
+
+	return newTransfer, nil
+}
--- app/domain/usecases/transfer/usecase.go
+++ app/domain/usecases/transfer/usecase.go
@@ -1,1 +1,16 @@
 package transferuc
+
+import (
+	"errors"
+
+	"github.com/jpgsaraceni/suricate-bank/app/domain/entities/transfer"
+)
+
+type Usecase struct {
+	Repository transfer.Repository
+}
+
+var (
+	ErrCreateTransfer           = errors.New("failed transfer")
+	ErrCreateTransferRepository = errors.New("repository error")
+)
