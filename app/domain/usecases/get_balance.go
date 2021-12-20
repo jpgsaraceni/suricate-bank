4941c73bade170d71e40c4927e26f6cd02aaf5e0
jpgsaraceni <jpgomes@id.uff.br>
2021-12-20 05:48:29 -0300

    test: Create tests for get_balance usecase

--- app/domain/usecases/get_balance.go
+++ app/domain/usecases/get_balance.go
@@ -3,6 +3,12 @@ package usecase
 import "github.com/jpgsaraceni/suricate-bank/app/domain/entities/account"
 
 func (uc Usecase) GetBalance(id account.AccountId) (int, error) {
+	balance, err := uc.Repository.GetBalance(id)
 
-	return uc.Repository.GetBalance(id)
+	if err != nil {
+
+		return 0, ErrInvalidId
+	}
+
+	return balance, nil
 }
--- app/domain/usecases/get_balance_test.go
+++ app/domain/usecases/get_balance_test.go
@@ -1,1 +1,80 @@
 package usecase
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/jpgsaraceni/suricate-bank/app/domain/entities/account"
+)
+
+func TestGetBalance(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		name       string
+		repository account.Repository
+		id         account.AccountId
+		want       int
+		err        error
+	}
+
+	var testUUID, _ = uuid.NewUUID()
+
+	testCases := []testCase{
+		{
+			name: "get 0 balance",
+			repository: account.MockRepository{
+				OnGetBalance: func(id account.AccountId) (int, error) {
+
+					return 0, nil
+				},
+			},
+			id:   account.AccountId(testUUID),
+			want: 0,
+		},
+		{
+			name: "get 100 balance",
+			repository: account.MockRepository{
+				OnGetBalance: func(id account.AccountId) (int, error) {
+
+					return 100, nil
+				},
+			},
+			id:   account.AccountId(testUUID),
+			want: 100,
+		},
+		{
+			name: "invalid Id",
+			repository: account.MockRepository{
+				OnGetBalance: func(id account.AccountId) (int, error) {
+
+					return 0, ErrInvalidId
+				},
+			},
+			id:   account.AccountId(uuid.Nil),
+			want: 0,
+			err:  ErrInvalidId,
+		},
+	}
+
+	for _, tt := range testCases {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			uc := Usecase{tt.repository}
+
+			newAccount, err := uc.GetBalance(tt.id)
+
+			if !errors.Is(err, tt.err) {
+				t.Fatalf("got %s expected %s", err, tt.err)
+			}
+
+			if !reflect.DeepEqual(newAccount, tt.want) {
+				t.Errorf("got %v expected %v", newAccount, tt.want)
+			}
+		})
+	}
+}
