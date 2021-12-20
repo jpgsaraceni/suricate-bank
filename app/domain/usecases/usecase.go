6ce8c1eeb3165b52b874a88b0222c0b0c2913fbc
jpgsaraceni <jpgomes@id.uff.br>
2021-12-20 05:47:53 -0300

    refactor: Separate err vars in usecase package

--- app/domain/usecases/usecase.go
+++ app/domain/usecases/usecase.go
@@ -1,8 +1,20 @@
 package usecase
 
-import "github.com/jpgsaraceni/suricate-bank/app/domain/entities/account"
+import (
+	"errors"
+
+	"github.com/jpgsaraceni/suricate-bank/app/domain/entities/account"
+)
 
 // Usecase calls Repository to be used in all methods of this package.
 type Usecase struct {
 	Repository account.Repository
 }
+
+var (
+	ErrCreateAccount = errors.New("failed to create account")
+	ErrNameLength    = errors.New("invalid name length")
+	ErrShortSecret   = errors.New("invalid password length")
+	ErrRepository    = errors.New("repository error")
+	ErrInvalidId     = errors.New("invalid id")
+)
