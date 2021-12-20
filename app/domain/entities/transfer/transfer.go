29003f292bd76ffa23664dd242fc06553ceb7bda
jpgsaraceni <jpgomes@id.uff.br>
2021-12-20 11:58:56 -0300

    feat: Create transfer entity and usecase

--- app/domain/entities/transfer/transfer.go
+++ app/domain/entities/transfer/transfer.go
@@ -1,1 +1,37 @@
 package transfer
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/jpgsaraceni/suricate-bank/app/domain/entities/account"
+)
+
+type (
+	TransferId uuid.UUID
+)
+
+type Transfer struct {
+	Id                   TransferId
+	AccountOriginId      account.AccountId
+	AccountDestinationId account.AccountId
+	Amount               account.Money // does this type make sense to come from account pkg?
+	CreatedAt            time.Time
+}
+
+func NewTransfer(amount account.Money, originId, destinationId account.AccountId) (Transfer, error) {
+	newTransfer := Transfer{
+		Id:                   newTransferId(),
+		AccountOriginId:      originId,
+		AccountDestinationId: destinationId,
+		Amount:               amount,
+		CreatedAt:            time.Now(),
+	}
+
+	return newTransfer, nil
+}
+
+func newTransferId() TransferId {
+
+	return TransferId(uuid.New())
+}
