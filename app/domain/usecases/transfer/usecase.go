3e12e4bcf240f81fd08842f330da4365729e00f0
jpgsaraceni <jpgomes@id.uff.br>
2021-12-21 12:48:45 -0300

    chore: Create Debiter and Crediter interfaces

--- app/domain/usecases/transfer/usecase.go
+++ app/domain/usecases/transfer/usecase.go
@@ -3,11 +3,23 @@ package transferuc
 import (
 	"errors"
 
+	"github.com/jpgsaraceni/suricate-bank/app/domain/entities/account"
 	"github.com/jpgsaraceni/suricate-bank/app/domain/entities/transfer"
+	"github.com/jpgsaraceni/suricate-bank/app/vos/money"
 )
 
 type Usecase struct {
 	Repository transfer.Repository
+	Crediter
+	Debiter
+}
+
+type Debiter interface {
+	Debit(id account.AccountId, amount money.Money) (account.Account, error)
+}
+
+type Crediter interface {
+	Credit(id account.AccountId, amount money.Money) (account.Account, error)
 }
 
 var (
