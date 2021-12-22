673373e4b95c17db8799e30c2d4fbf39b7ebc04b
jpgsaraceni <jpgomes@id.uff.br>
2021-12-22 10:36:18 -0300

    feat: Implement SAGA pattern in create_transfer uc

--- app/domain/usecases/transfer/create_transfer.go
+++ app/domain/usecases/transfer/create_transfer.go
@@ -1,12 +1,38 @@
 package transferuc
 
 import (
+	"errors"
+
 	"github.com/jpgsaraceni/suricate-bank/app/domain/entities/account"
 	"github.com/jpgsaraceni/suricate-bank/app/domain/entities/transfer"
 	"github.com/jpgsaraceni/suricate-bank/app/vos/money"
 )
 
+// tudo bem ter esse erro repetido na entidade e na usecase?
+var errSameAccounts = errors.New("origin and destination must be different accounts")
+
 func (uc Usecase) Create(amount money.Money, originId, destinationId account.AccountId) (transfer.Transfer, error) {
+
+	if originId == destinationId {
+
+		return transfer.Transfer{}, errSameAccounts
+	}
+
+	err := uc.Debiter.Debit(originId, amount) // deveria não retornar a conta?
+
+	if err != nil {
+
+		return transfer.Transfer{}, err // TODO translate this error
+	}
+
+	// doesn't exist, negative or zero amount
+	err = uc.Crediter.Credit(destinationId, amount)
+
+	if err != nil {
+		uc.Crediter.Credit(originId, amount) // deveria tratar o erro que pode retornar aqui?
+
+		return transfer.Transfer{}, err
+	}
 
 	newTransfer, err := transfer.NewTransfer(amount, originId, destinationId)
 
