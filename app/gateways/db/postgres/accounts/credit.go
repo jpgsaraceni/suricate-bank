f87d6d8e6034e55455c19c34ff09e10b84bb041f
jpgsaraceni <jpgomes@id.uff.br>
2022-01-03 10:13:17 -0300

    refactor: Change paramater of credit and debit

--- app/gateways/db/postgres/accounts/credit.go
+++ app/gateways/db/postgres/accounts/credit.go
@@ -2,23 +2,24 @@ package accountspg
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/jpgsaraceni/suricate-bank/app/domain/entities/account"
 	"github.com/jpgsaraceni/suricate-bank/app/vos/money"
 )
 
-func (r Repository) CreditAccount(account *account.Account, amount money.Money) error {
+func (r Repository) CreditAccount(ctx context.Context, id account.AccountId, amount money.Money) error {
 	const query = `
 		UPDATE accounts
 		SET balance = balance + $1
 		WHERE id = $2;
 	`
 
-	err := r.pool.QueryRow(context.TODO(), query, amount.Cents(), account.Id)
+	_, err := r.pool.Exec(ctx, query, amount.Cents(), id)
 
 	if err != nil {
 
-		return errQuery
+		return fmt.Errorf("%w: %s", errQuery, err.Error())
 	}
 
 	return nil
