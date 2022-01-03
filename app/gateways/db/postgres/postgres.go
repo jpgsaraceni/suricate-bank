6eba5f679460fc33474c92b8d01fef18297c33f6
jpgsaraceni <jpgomes@id.uff.br>
2022-01-02 22:32:12 -0300

    refactor: Wrap error

--- app/gateways/db/postgres/postgres.go
+++ app/gateways/db/postgres/postgres.go
@@ -3,20 +3,29 @@ package postgres
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
-var errConfigureDb = errors.New("failed to configure db connection")
+var (
+	errConfigureDb = errors.New("failed to configure db connection")
+	errConnectDb   = errors.New("failed to connect to db")
+)
 
 func ConnectPool(url string) (*pgxpool.Pool, error) {
 	config, err := pgxpool.ParseConfig(url)
 
 	if err != nil {
 
-		return nil, errConfigureDb
+		return nil, fmt.Errorf("%w: %s", errConfigureDb, err.Error())
 	}
 
 	pool, err := pgxpool.ConnectConfig(context.TODO(), config)
-	return pool, err
+
+	if err != nil {
+		return nil, fmt.Errorf("%w: %s", errConnectDb, err.Error())
+	}
+
+	return pool, nil
 }
