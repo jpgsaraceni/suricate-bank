1e9cadc108163ba4f55d6b33558d257a651a90ef
jpgsaraceni <jpgomes@id.uff.br>
2022-01-05 14:53:32 -0300

    chore: Call usecase in handler

--- app/gateways/api/http/accounts/create.go
+++ app/gateways/api/http/accounts/create.go
@@ -2,6 +2,7 @@ package accountsroute
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/jpgsaraceni/suricate-bank/app/gateways/api/http/responses"
@@ -18,6 +19,31 @@ func (h Handler) Create(r *http.Request) responses.Response {
 
 	if err := json.NewDecoder(r.Body).Decode(&createRequest); err != nil {
 		return responses.BadRequest(err, ErrMissingFields)
+	}
+
+	cpf := createRequest.Cpf
+
+	if len(cpf) != 11 && len(cpf) != 14 {
+
+		return responses.BadRequest(fmt.Errorf("invalid cpf length"), ErrLengthCpf)
+	}
+
+	name := createRequest.Name
+
+	if len(name) < 3 {
+
+		return responses.BadRequest(fmt.Errorf("name too short"), ErrShortName)
+	}
+
+	secret := createRequest.Secret
+
+	if len(secret) < 6 {
+
+		return responses.BadRequest(fmt.Errorf("secret too short"), ErrShortSecret)
+	}
+
+	if _, err := h.Usecase.Create(r.Context(), name, cpf, secret); err != nil {
+		return responses.InternalServerError(err, ErrInternalServerError)
 	}
 
 	return responses.Response{}
--- app/gateways/api/http/accounts/handler.go
+++ app/gateways/api/http/accounts/handler.go
@@ -1,5 +1,8 @@
 package accountsroute
 
+import accountuc "github.com/jpgsaraceni/suricate-bank/app/domain/usecases/account"
+
 // Handler will be used to bind all handlers for the /accounts route,
 type Handler struct {
+	Usecase accountuc.Usecase
 }
