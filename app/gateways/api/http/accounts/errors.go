e47aad3c88e2349ecf81d60020f43d13149398d9
jpgsaraceni <jpgomes@id.uff.br>
2022-01-05 14:53:47 -0300

    chore: Add error variables

--- app/gateways/api/http/accounts/errors.go
+++ app/gateways/api/http/accounts/errors.go
@@ -1,5 +1,13 @@
 package accountsroute
 
-import "github.com/jpgsaraceni/suricate-bank/app/gateways/api/http/responses"
+import (
+	"github.com/jpgsaraceni/suricate-bank/app/gateways/api/http/responses"
+)
 
-var ErrMissingFields = responses.ErrorPayload{Message: "missing fields: name, cpf and/or secret"}
+var (
+	ErrMissingFields       = responses.ErrorPayload{Message: "missing fields: name, cpf and/or secret"}
+	ErrLengthCpf           = responses.ErrorPayload{Message: "invalid cpf length"}
+	ErrShortName           = responses.ErrorPayload{Message: "name too short"}
+	ErrShortSecret         = responses.ErrorPayload{Message: "password too short"}
+	ErrInternalServerError = responses.ErrorPayload{Message: "internal server error"}
+)
