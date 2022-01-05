6fc1ee0ee75a9f5e727f11c6833820ccd40f562f
jpgsaraceni <jpgomes@id.uff.br>
2022-01-05 14:54:03 -0300

    chore: Add response functions

--- app/gateways/api/http/responses/responses.go
+++ app/gateways/api/http/responses/responses.go
@@ -23,6 +23,21 @@ func BadRequest(err error, errorPayload ErrorPayload) Response {
 	}
 }
 
+func InternalServerError(err error, errorPayload ErrorPayload) Response {
+	return Response{
+		Status:  http.StatusInternalServerError,
+		Error:   err,
+		Payload: errorPayload,
+	}
+}
+
+func Created(payload string) Response {
+	return Response{
+		Status:  http.StatusCreated,
+		Payload: payload,
+	}
+}
+
 func SendJSON(w http.ResponseWriter, response Response) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(response.Status)
