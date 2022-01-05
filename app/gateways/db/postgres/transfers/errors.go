47c561b5394890b8ee99a94a61c5bddbf6dcfe3a
jpgsaraceni <jpgomes@id.uff.br>
2022-01-05 10:55:08 -0300

    chore: Create error variables for transfers

---
+++ app/gateways/db/postgres/transfers/errors.go
@@ -0,0 +1,9 @@
+package transferspg
+
+import "errors"
+
+var (
+	ErrQuery        = errors.New("failed to run query")
+	ErrScanningRows = errors.New("failed to scan rows returned from query")
+	ErrParse        = errors.New("failed to parse query return")
+)
