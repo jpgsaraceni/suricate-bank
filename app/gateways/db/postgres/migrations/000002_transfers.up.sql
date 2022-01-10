fdf3608fd1e466c1c230c1e41eda97ff4c04fc23
jpgsaraceni <jpgomes@id.uff.br>
2022-01-10 18:09:46 -0300

    refactor: Add no same account transfer constraint

--- app/gateways/db/postgres/migrations/000002_transfers.up.sql
+++ app/gateways/db/postgres/migrations/000002_transfers.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS transfers
 (
     id UUID PRIMARY KEY,
     account_origin_id UUID NOT NULL REFERENCES accounts (id),
-    account_destination_id UUID NOT NULL REFERENCES accounts (id),
+    account_destination_id UUID NOT NULL REFERENCES accounts (id) CHECK (account_destination_id != account_origin_id),
     amount INT NOT NULL CHECK (amount > 0),
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT(now())
 );
