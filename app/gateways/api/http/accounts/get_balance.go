a4023e727448ab27b88fa0820968d1c86a27be04
jpgsaraceni <jpgomes@id.uff.br>
2022-01-12 16:34:03 -0300

    chore: Update get balance handler

--- app/gateways/api/http/accounts/get_balance.go
+++ app/gateways/api/http/accounts/get_balance.go
@@ -1,32 +1,46 @@
 package accountsroute
 
-// import (
-// 	"net/http"
-// 	"strings"
+import (
+	"errors"
+	"net/http"
+	"strings"
 
-// 	"github.com/google/uuid"
+	"github.com/google/uuid"
 
-// 	"github.com/jpgsaraceni/suricate-bank/app/domain/entities/account"
-// 	"github.com/jpgsaraceni/suricate-bank/app/gateways/api/http/responses"
-// )
+	"github.com/jpgsaraceni/suricate-bank/app/domain/entities/account"
+	accountuc "github.com/jpgsaraceni/suricate-bank/app/domain/usecases/account"
+	"github.com/jpgsaraceni/suricate-bank/app/gateways/api/http/responses"
+)
 
-// func (h handler) GetBalance(w http.ResponseWriter, r *http.Request) {
-// 	p := strings.Split(r.URL.Path, "/")
-// 	id, err := uuid.Parse(p[1])
+func (h handler) GetBalance(w http.ResponseWriter, r *http.Request) {
+	p := strings.Split(r.URL.Path, "/")
+	id, err := uuid.Parse(p[1])
 
-// 	if err != nil {
-// 		RequestError(w, responses.ErrInvalidPathParameter)
+	var response responses.Response
 
-// 		return
-// 	}
+	defer func(r *responses.Response) {
+		responses.SendJSON(w, *r)
+	}(&response)
 
-// 	balance, err := h.usecase.GetBalance(r.Context(), account.AccountId(id))
+	if err != nil {
+		response = responses.BadRequest(responses.ErrInvalidPathParameter)
 
-// 	if err != nil {
-// 		RequestError(w, err)
+		return
+	}
 
-// 		return
-// 	}
+	balance, err := h.usecase.GetBalance(r.Context(), account.AccountId(id))
 
-// 	responses.SendJSON(w, responses.GotBalance(balance))
-// }
+	if errors.Is(err, accountuc.ErrIdNotFound) {
+		response = responses.BadRequest(responses.ErrAccountNotFound)
+
+		return
+	}
+
+	if err != nil {
+		response = responses.InternalServerError(err)
+
+		return
+	}
+
+	response = responses.GotBalance(balance)
+}
