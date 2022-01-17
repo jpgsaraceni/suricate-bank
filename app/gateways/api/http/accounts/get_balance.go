d18b1a89c0eed3dcca864ccdbc14e24a5c4e5cc7
jpgsaraceni <jpgomes@id.uff.br>
2022-01-17 09:43:01 -0300

    refactor: Use regex to get path param

--- app/gateways/api/http/accounts/get_balance.go
+++ app/gateways/api/http/accounts/get_balance.go
@@ -3,6 +3,7 @@ package accountsroute
 import (
 	"errors"
 	"net/http"
+	"regexp"
 	"strings"
 
 	"github.com/google/uuid"
@@ -16,7 +17,7 @@ func (h handler) GetBalance(w http.ResponseWriter, r *http.Request) {
 	var response responses.Response
 	response.Writer = w
 
-	accountId, err := getAccountIdFromPath(r)
+	accountId, err := getAccountIdFromRequest(r)
 
 	if err != nil {
 		response.BadRequest(responses.ErrInvalidPathParameter).SendJSON()
@@ -27,7 +28,7 @@ func (h handler) GetBalance(w http.ResponseWriter, r *http.Request) {
 	balance, err := h.usecase.GetBalance(r.Context(), accountId)
 
 	if errors.Is(err, accountuc.ErrIdNotFound) {
-		response.BadRequest(responses.ErrAccountNotFound).SendJSON()
+		response.NotFound(responses.ErrAccountNotFound).SendJSON()
 
 		return
 	}
@@ -41,10 +42,8 @@ func (h handler) GetBalance(w http.ResponseWriter, r *http.Request) {
 	response.Ok(responses.GotBalancePayload(balance)).SendJSON()
 }
 
-func getAccountIdFromPath(r *http.Request) (account.AccountId, error) {
-	// const pathParamPattern = "/accounts/{account_id}/balance"
-	// idParam := getPathParam(r.URL.Path, pathParamPattern)
-	idParam := getPathParam(r.URL.Path, 2)
+func getAccountIdFromRequest(r *http.Request) (account.AccountId, error) {
+	idParam := getPathParam(r.URL.Path)
 	parsedToUuid, err := uuid.Parse(idParam)
 
 	if err != nil {
@@ -55,7 +54,24 @@ func getAccountIdFromPath(r *http.Request) (account.AccountId, error) {
 	return account.AccountId(parsedToUuid), nil
 }
 
-func getPathParam(url string, position int) string { // TODO: define position using regexp
+func getPathParam(url string) string {
 	parts := strings.Split(url, "/")
+	position := getPathParamPosition()
+
 	return parts[position]
+}
+
+const pathPattern = "/accounts/{account_id}/balance"
+
+func getPathParamPosition() int {
+	re := regexp.MustCompile(`\{\w*\}`)
+	parts := strings.Split(pathPattern, "/")
+	for i := range parts {
+		if re.MatchString(parts[i]) {
+
+			return i
+		}
+	}
+
+	return 0
 }
