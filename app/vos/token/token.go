6053ed17225e63fbdb02dfa54184be91fd57a52a
jpgsaraceni <jpgomes@id.uff.br>
2022-01-19 16:56:52 -0300

    feat: Create token sign function

--- app/vos/token/token.go
+++ app/vos/token/token.go
@@ -1,18 +1,55 @@
 package token
 
-import "github.com/jpgsaraceni/suricate-bank/app/domain/entities/account"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"time"
 
-// receive accountId and return token
-// receive token and return accountId
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/joho/godotenv"
+
+	"github.com/jpgsaraceni/suricate-bank/app/domain/entities/account"
+)
+
+var ErrSignJWT = errors.New("failed to sign jwt")
 
 type Jwt struct {
 	token string
 }
 
-func GenerateJWT(accountId account.AccountId) Jwt {
-	return Jwt{token: ""} // TODO: implement jwt
-}
-
 func (j Jwt) Value() string {
 	return j.token
+}
+
+type jwtClaimsSchema struct {
+	AccountId string `json:"account_id"`
+	jwt.RegisteredClaims
+}
+
+func Sign(accountId account.AccountId) (Jwt, error) {
+
+	claims := jwtClaimsSchema{
+		accountId.String(),
+		jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Minute * 30)),
+			Issuer:    "suricate bank",
+		},
+	}
+
+	unsignedToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	signedToken, err := unsignedToken.SignedString(loadSecret())
+
+	if err != nil {
+
+		return Jwt{}, fmt.Errorf("%w: %s", ErrSignJWT, err)
+	}
+
+	return Jwt{token: signedToken}, nil
+}
+
+func loadSecret() []byte {
+	godotenv.Load()
+	return []byte(os.Getenv("JWT_SECRET"))
 }
--- app/vos/token/token_test.go
+++ app/vos/token/token_test.go
@@ -1,3 +1,62 @@
 package token
 
-//TODO: escrever testes
+import (
+	"errors"
+	"log"
+	"testing"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/google/uuid"
+
+	"github.com/jpgsaraceni/suricate-bank/app/domain/entities/account"
+)
+
+func TestGenerateJWT(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		name string
+		id   account.AccountId
+		want string
+		err  error
+	}
+
+	var testId = account.AccountId(uuid.New())
+
+	testCases := []testCase{
+		{
+			name: "successfully generate token",
+			id:   testId,
+			want: testId.String(),
+		},
+	}
+
+	for _, tt := range testCases {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			generatedToken, err := Sign(tt.id)
+
+			if !errors.Is(err, tt.err) {
+				t.Errorf("got error %s expected error %s", err, tt.err)
+			}
+
+			parsedToken, _ := jwt.ParseWithClaims(generatedToken.Value(), &jwtClaimsSchema{}, func(token *jwt.Token) (interface{}, error) {
+				return loadSecret(), nil
+			})
+
+			claims, ok := parsedToken.Claims.(*jwtClaimsSchema)
+
+			if ok && parsedToken.Valid {
+				log.Printf("%v %v", claims.AccountId, claims.RegisteredClaims.ExpiresAt)
+			} else {
+				t.Fatal("failed to parse token")
+			}
+
+			if claims.AccountId != tt.want {
+				t.Errorf("got %s expected %s", claims.AccountId, tt.want)
+			}
+		})
+	}
+}
