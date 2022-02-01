916efe46cb0644479321aa326997cff4f5427f14
jpgsaraceni <jpgomes@id.uff.br>
2022-02-01 10:18:17 -0300

    chore: Add file driver for migration

--- app/gateways/db/postgres/migrate.go
+++ app/gateways/db/postgres/migrate.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/golang-migrate/migrate/v4/source/github"
 )
 
