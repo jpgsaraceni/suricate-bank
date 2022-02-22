68189209c56c2c0e5d1b96e68585a1c2fcaca3b4
jpgsaraceni <jpgomes@id.uff.br>
2022-02-22 09:46:11 -0300

    refactor: Simplify request logger middleware

--- app/gateways/api/http/middlewares/logger.go
+++ app/gateways/api/http/middlewares/logger.go
@@ -8,23 +8,21 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-func RequestLogger() func(next http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		loggerHandler := hlog.NewHandler(log.Logger)
-		requestHandler := hlog.AccessHandler(func(r *http.Request, status, size int, duration time.Duration) {
-			hlog.FromRequest(r).Info().
-				Str("method", r.Method).
-				Stringer("url", r.URL).
-				Str("ua", r.Header.Get("user-agent")).
-				Str("ip", r.RemoteAddr).
-				Str("referer", r.Referer()).
-				// TODO: log requestID
-				Int("status", status).
-				Int("size", size).
-				Dur("duration", duration).
-				Msg("")
-		})
+func RequestLogger(next http.Handler) http.Handler {
+	loggerHandler := hlog.NewHandler(log.Logger)
+	requestHandler := hlog.AccessHandler(func(r *http.Request, status, size int, duration time.Duration) {
+		hlog.FromRequest(r).Info().
+			Str("method", r.Method).
+			Stringer("url", r.URL).
+			Str("ua", r.Header.Get("user-agent")).
+			Str("ip", r.RemoteAddr).
+			Str("referer", r.Referer()).
+			Str("req_id", GetReqID(r.Context())).
+			Int("status", status).
+			Int("size", size).
+			Dur("duration", duration).
+			Msg("")
+	})
 
-		return loggerHandler(requestHandler(next))
-	}
+	return loggerHandler(requestHandler(next))
 }
